717a53f76467ab773b9bdc1d6341fc6f544ffc2d
ClementMadiot <clementmadiot09@gmail.com>
2024-06-12 12:02:10 +0200

    done Beneficts section

--- src/components/Beneficts.jsx
+++ src/components/Beneficts.jsx
@@ -1,5 +1,13 @@
+// components
 import Heading from "./Heading";
 import Section from "./Section";
+// array
+import { benefits } from "../constant";
+// design
+import { GradientLight } from "./design/Benefits";
+// assets
+import Arrow from "../assets/svg/Arrow";
+import ClipPath from "../assets/svg/ClipPath"
 
 const Beneficts = () => {
   return (
@@ -9,6 +17,51 @@ const Beneficts = () => {
           className="md:max-w-md lg:max-w-2xl"
           title="Chat Smarter, Not Harder with Brainwave"
         />
+        <div className="flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <article
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{ backgroundImage: `url(${item.backgroundUrl})` }}
+              key={item.id}
+            >
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h2 mb-5">{item.title}</h5>
+                <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                <div className="flex items-center mt-auto">
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    explore more
+                  </p>
+                  <Arrow />
+                </div>
+              </div>
+
+              {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+              <ClipPath/>
+            </article>
+          ))}
+        </div>
       </div>
     </Section>
   );
