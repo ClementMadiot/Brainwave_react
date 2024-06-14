81353487737e312b72ce48a43cbcd29119581bdd
ClementMadiot <clementmadiot09@gmail.com>
2024-06-14 11:53:49 +0200

    add button roadmap

--- src/components/Roadmap/Roadmap.jsx
+++ src/components/Roadmap/Roadmap.jsx
@@ -4,6 +4,7 @@ import Section from "../Section";
 import { roadmap } from "../../constant";
 import { check2, grid, loading1 } from "../../assets";
 import TagLine from "../TagLine";
+import Button from "../Button";
 
 const Roadmap = () => {
   return (
@@ -14,8 +15,14 @@ const Roadmap = () => {
         <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
           {roadmap.map((item) => {
             const status = item.status === "done" ? "Done" : "In progress";
+
             return (
-              <div key={item.id} className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${item.colorful ? "bg-conic-gradient" : "bg-n-6"}`}>
+              <div
+                key={item.id}
+                className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
+                  item.colorful ? "bg-conic-gradient" : "bg-n-6"
+                }`}
+              >
                 <div className="relative p-8 bg-n-8 rounded-[2.5rem] overflow-hidden xl:p-15">
                   <article className="absolute top-0 left-0 max-w-full">
                     <img
@@ -27,37 +34,43 @@ const Roadmap = () => {
                     />
                   </article>
 
-                  <div className="relative z-1"></div>
-                  <article className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
-                    <TagLine>{item.date}</TagLine>
+                  <div className="relative z-1">
+                    <article className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
+                      <TagLine>{item.date}</TagLine>
 
-                    <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
+                      <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
+                        <img
+                          src={item.status === "done" ? check2 : loading1}
+                          className="mr-2.5"
+                          height={16}
+                          width={16}
+                          alt={status}
+                        />
+                        <div className="tagline">{status}</div>
+                      </div>
+                    </article>
+
+                    <article className="mb-10 -my-10 -mx-15">
                       <img
-                        src={item.status === "done" ? check2 : loading1}
-                        className="mr-2.5"
-                        height={16}
-                        width={16}
-                        alt={status}
+                        src={item.imageUrl}
+                        className="w-full"
+                        width={630}
+                        height={420}
+                        alt={item.title}
                       />
-                      <div className="tagline">{status}</div>
-                    </div>
-                  </article>
+                    </article>
 
-                  <article className="mb-10 -my-10 -mx-15">
-                    <img src={item.imageUrl}
-                    className="w-full"
-                    width={630}
-                    height={420}
-                    alt={item.title} />
-                  </article>
-
-                  <h4 className="h4 mb-4">{item.title}</h4>
-                  <p className="body-2 text-n-4">{item.text}</p>
-
+                    <h4 className="h4 mb-4">{item.title}</h4>
+                    <p className="body-2 text-n-4">{item.text}</p>
+                  </div>
                 </div>
               </div>
             );
           })}
+        </div>
+
+        <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
+          <Button href="/roadmap">Our roadmap</Button>
         </div>
       </div>
     </Section>
